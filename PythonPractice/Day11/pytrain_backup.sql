1df2deac98ddacd34368491566b613cfa44c126c
Unnathi <unnathi.k@perfios.com>
2022-11-11 12:09:42 +0530

    Python Practicefiles

---
+++ PythonPractice/Day11/pytrain_backup.sql
@@ -0,0 +1,77 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Linux (x86_64)
+--
+-- Host: localhost    Database: pytrain
+-- ------------------------------------------------------
+-- Server version	8.0.31-0ubuntu0.20.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `emp`
+--
+
+DROP TABLE IF EXISTS `emp`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `emp` (
+  `empid` int NOT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  `mgrid` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `emp`
+--
+
+LOCK TABLES `emp` WRITE;
+/*!40000 ALTER TABLE `emp` DISABLE KEYS */;
+INSERT INTO `emp` VALUES (10,'A',20),(20,'B',30),(30,'C',10),(10,'A',20),(20,'B',30),(30,'C',10),(10,'A',20),(20,'B',30),(30,'C',10),(40,'D',15);
+/*!40000 ALTER TABLE `emp` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student` (
+  `name` varchar(40) NOT NULL,
+  `branch` varchar(60) DEFAULT NULL,
+  `roll` int NOT NULL,
+  `section` varchar(40) DEFAULT NULL,
+  `age` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` VALUES ('Joshua','CSE',17,'K',25),('Vernon','CSE',18,'A',22),('Scoups','ISE',19,'B',27),('Mingyu','Mech.',20,'C',25);
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-29 10:11:02
---
+++ PythonPractice/Day12/MongoDB_Practice.js
@@ -0,0 +1,15 @@
+use unnathidb
+
+// db.staff.insertMany([
+// {"contain_id":1,name:"Joshua"},
+// {"contain_id":2,name:"Vernon",address:"Canada"},
+// {"contain_id":3,name:"Dilip", mobile_no:1234567890},
+// {"contain_id":4,name:"Venky",age:22},
+// {"contain_id":5,name:"Jessi",age:22,emailid:"jessils@gmail.com"}]);
+
+
+// db.staff.find( {age:22} );
+
+// db.staff.deleteOne({ contain_id:3}); 
+// show dbs
+db.staff.find()
